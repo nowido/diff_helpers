800df8650fdb9da62290be0d6eb11e7ba68e350f
nowido <nowido@mail.ru>
2017-01-10 09:45:41 +0300

    Add files via upload

---
+++ OpenclTests/setenv.bat
@@ -0,0 +1,3 @@
+rem set PATH=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin;%PATH%
+set INCLUDE=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\include;%INCLUDE%
+set LIB=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\lib\x64;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\lib\Win32;%LIB%
