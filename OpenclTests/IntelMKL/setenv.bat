90a521b2593379b5c7703f4de0f48fb66e01b4aa
nowido <nowido@mail.ru>
2017-01-30 11:08:38 +0300

    Add files via upload

---
+++ OpenclTests/IntelMKL/setenv.bat
@@ -0,0 +1,7 @@
+rem set PATH=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin;%PATH%
+rem set PATH=C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2017.0.109\windows\redist\intel64_win\tbb;%PATH%
+set INCLUDE=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\include;%INCLUDE%
+set INCLUDE=C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2017.0.109\windows\tbb\include;C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2017.0.109\windows\mkl\include;%INCLUDE%
+set LIB=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\lib\x64;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\lib\Win32;%LIB%
+set LIB=C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2017.0.109\windows\tbb\lib\intel64\vc14;C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2017.0.109\windows\mkl\lib\intel64;%LIB%
+set LIB=C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2017.0.109\windows\compiler\lib\intel64;%LIB%
