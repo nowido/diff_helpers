20aac8885bbd18b05703efec931e580a20271969
nowido <nowido@mail.ru>
2017-02-01 14:25:01 +0300

    Update comptbb.sh

--- OpenclTests/IntelMKL/comptbb.sh
+++ OpenclTests/IntelMKL/comptbb.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-g++ -std=c++11 -otesttbb -I${TBBROOT}/include testtbb.cpp -L${TBBROOT}/lib -ltbb
+#g++ -std=c++11 -otesttbb -I${TBBROOT}/include testtbb.cpp -L${TBBROOT}/lib -ltbb
+g++ -std=c++11 -m64 -Ofast -march=native -funroll-loops -otesttbb -I${TBBROOT}/include testtbb.cpp -L${TBBROOT}/lib -ltbb
