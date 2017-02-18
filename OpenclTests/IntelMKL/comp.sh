f68b9000a4e8bced0472d1ed0a015ee223e25b9a
nowido <nowido@mail.ru>
2017-02-18 16:58:53 +0300

    Update comp.sh

--- OpenclTests/IntelMKL/comp.sh
+++ OpenclTests/IntelMKL/comp.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-g++ -otmkld  -DMKL_ILP64 -m64 -I${MKLROOT}/include tmkld.cpp ${MKLROOT}/lib/libmkl_intel_ilp64.a ${MKLROOT}/lib/libmkl_tbb_thread.a ${MKLROOT}/lib/libmkl_core.a -ltbb -lstdc++ -lpthread -lm -ldl -L/opt/intel/tbb/lib
+g++ -otmkl  -DMKL_ILP64 -std=c++11 -mavx -Ofast -march=haswell -funroll-loops -I${MKLROOT}/include tmkl.cpp ${MKLROOT}/lib/libmkl_intel_ilp64.a ${MKLROOT}/lib/libmkl_tbb_thread.a ${MKLROOT}/lib/libmkl_core.a -ltbb -lstdc++ -lpthread -lm -ldl -L/opt/intel/tbb/lib
+#g++ -otmkld  -DMKL_ILP64 -std=c++11 -mavx -Ofast -march=haswell -funroll-loops -I${MKLROOT}/include tmkld.cpp ${MKLROOT}/lib/libmkl_intel_ilp64.a ${MKLROOT}/lib/libmkl_tbb_thread.a ${MKLROOT}/lib/libmkl_core.a -ltbb -lstdc++ -lpthread -lm -ldl -L/opt/intel/tbb/lib
